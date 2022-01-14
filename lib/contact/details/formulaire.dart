6d64c50f885f6a85aa92383a8f634ed6502cfd88
Amaury <mr.amaury@hotmail.fr>
2022-01-14 14:31:19 +0100

    [ADD] Ajout d'un client a partir du formulaire et Required des parametres listclient et client

--- lib/contact/details/formulaire.dart
+++ lib/contact/details/formulaire.dart
@@ -1,164 +1,251 @@
 import 'package:flutter/material.dart';
+import 'package:gestionnaire_de_client/contact/View/ViewListClients.dart';
 import 'package:gestionnaire_de_client/contact/model/client.dart';
+import 'package:get/get.dart';
 
 class FormClient extends StatelessWidget {
-  final Client? client;
-
-  const FormClient({Key? key, this.client}) : super(key: key);
+  final Client client;
+  final RxList<Client> listClients;
+  final String title;
+  final _formKey = GlobalKey<FormState>();
+  FormClient(
+      {Key? key,
+      required this.title,
+      required this.client,
+      required this.listClients})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(client?.firstName ?? 'Nouveau client'),
+        title: Text(client.firstName),
         backgroundColor: Colors.grey,
-        actions: const <Widget>[
-          Icon(
-            Icons.task_alt_outlined,
-            color: Colors.green,
+        actions: <Widget>[
+          ElevatedButton(
+            child: const Icon(
+              Icons.task_alt_outlined,
+              color: Colors.green,
+            ),
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                listClients.add(client);
+                Get.to(
+                  ClientListView(
+                    listClients: listClients,
+                  ),
+                );
+              }
+            },
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
-            child: Container(
-              width: 700,
-              height: 235,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 1,
-                ),
-              ),
-              child: Column(
-                children: [
-                  const Text(
-                    'Informations',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+              child: Container(
+                width: 700,
+                height: 235,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1,
                   ),
-                  Row(
-                    children: <Widget>[
-                      Flexible(
-                        child: TextField(
+                ),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Informations',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 23),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Flexible(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              border: const OutlineInputBorder(),
+                              hintText: client.lastName,
+                            ),
+                            onSaved: (lastName) =>
+                                client.lastName = lastName ?? "",
+                            validator: (String? value) {
+                              if (value != null &&
+                                  value != "Nom" &&
+                                  value != "") {
+                                client.lastName = value;
+                              } else {
+                                return 'Nom obligatoire';
+                              }
+                            },
+                          ),
+                        ),
+                        Flexible(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              border: const OutlineInputBorder(),
+                              hintText: client.firstName,
+                            ),
+                            onSaved: (firstName) =>
+                                client.lastName = firstName ?? "",
+                            validator: (String? value) {
+                              if (value != null &&
+                                  value != "Prénom" &&
+                                  value != "") {
+                                client.firstName = value;
+                              } else {
+                                return 'Prénom obligatoire';
+                              }
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 7),
+                      child: TextFormField(
                           decoration: InputDecoration(
                             border: const OutlineInputBorder(),
-                            hintText: client?.lastName ?? "Nom",
+                            hintText: client.societe,
+                          ),
+                          onSaved: (societe) => client.societe = societe ?? "",
+                          validator: (String? value) {
+                            if (value != null && value != "Société") {
+                              client.societe = value;
+                            }
+                            return null;
+                          }),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 7),
+                      child: TextFormField(
+                          decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            hintText: client.poste,
+                          ),
+                          onSaved: (poste) => client.poste = poste ?? "",
+                          validator: (String? value) {
+                            if (value != null && value != "Poste") {
+                              client.poste = value;
+                            }
+                            return null;
+                          }),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+              child: Container(
+                width: 700,
+                height: 325,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1,
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Coordonnés',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 23),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 7),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          hintText: client.telPro,
+                          prefixIcon: const Icon(
+                            Icons.phone_outlined,
                           ),
                         ),
                       ),
-                      Flexible(
-                        child: TextField(
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 7),
+                      child: TextFormField(
                           decoration: InputDecoration(
                             border: const OutlineInputBorder(),
-                            hintText: client?.firstName ?? 'Prenom',
+                            hintText: client.tel,
+                            prefixIcon: const Icon(
+                              Icons.smartphone_outlined,
+                            ),
                           ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 10, vertical: 7),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        hintText: client?.societe ?? 'Société',
-                      ),
+                          onSaved: (tel) => client.tel = tel ?? "",
+                          validator: (String? value) {
+                            if (value != null && value != "Téléphone") {
+                              client.tel = value;
+                            } else if (value != null &&
+                                value.contains(RegExp(r'[A-Z]'))) {
+                              return "Le numéro de téléphone ne doit pas contenir de lettre";
+                            } else {
+                              return null;
+                            }
+                          }),
                     ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 10, vertical: 7),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        hintText: client?.poste ?? 'Poste',
-                      ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 7),
+                      child: TextFormField(
+                          decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            hintText: client.fax,
+                            prefixIcon: const Icon(
+                              Icons.print_outlined,
+                            ),
+                          ),
+                          onSaved: (telPro) => client.telPro = telPro ?? "",
+                          validator: (String? value) {
+                            if (value != null &&
+                                value != "Téléphone professionnel") {
+                              client.telPro = value;
+                            } else if (value != null &&
+                                value.contains(RegExp(r'[A-Z]'))) {
+                              return "Le numéro de téléphone ne doit pas contenir de lettre";
+                            }
+                            return null;
+                          }),
                     ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
-            child: Container(
-              width: 700,
-              height: 325,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 1,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 7),
+                      child: TextFormField(
+                          decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            hintText: client.email,
+                            prefixIcon: const Icon(
+                              Icons.email_outlined,
+                            ),
+                          ),
+                          onSaved: (email) => client.email = email ?? "",
+                          validator: (String? value) {
+                            if (value != null && value != "Email") {
+                              client.email = value;
+                            } else if (value != null && !value.contains('@')) {
+                              return "L'email doit contenir un @";
+                            }
+                            return null;
+                          }),
+                    ),
+                  ],
                 ),
               ),
-              child: Column(
-                children: [
-                  const Text(
-                    'Coordonnés',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 10, vertical: 7),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        hintText:
-                            client?.tel ?? 'Numéro de téléphone professionel ',
-                        prefixIcon: const Icon(
-                          Icons.phone_outlined,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 10, vertical: 7),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        hintText: client?.tel ?? 'Numéro de téléphone',
-                        prefixIcon: const Icon(
-                          Icons.smartphone_outlined,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 10, vertical: 7),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        hintText: client?.fax ?? 'Fax',
-                        prefixIcon: const Icon(
-                          Icons.print_outlined,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 10, vertical: 7),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        hintText: client?.email ?? 'Email',
-                        prefixIcon: const Icon(
-                          Icons.email_outlined,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
